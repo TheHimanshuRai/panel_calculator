ee2648285582afa5edb2ea98f2d889af9f784ee4
TheHimanshuRai <103231489+TheHimanshuRai@users.noreply.github.com>
2023-08-28 09:53:11 +0530

    Update run_server.sh

--- run_server.sh
+++ run_server.sh
@@ -1,3 +1,3 @@
 # assume that the container already has activated the right environment
 # asumme current working directory is at the top level of this repo
-panel serve stocks/stocks_hvplot.ipynb --address 0.0.0.0 --port 80 --allow-websocket-origin="*"
+panel serve calculator/panel_trial.ipynb --address 0.0.0.0 --port 80 --allow-websocket-origin="*"
